abe4a15e4ad4b0e4291cac27fb6671121cfa0e5b
Juan Ledesma <jledesmam@yahoo.es>
2021-05-25 15:11:12 +0100

    provisional script to rename fasta files and transfer them to a new folder

--- python_script/rename_headers_and_fasta_files.py
+++ python_script/rename_headers_and_fasta_files.py
@@ -1,20 +1,19 @@
-import os, sys, re
-folder =sys.argv[1]
-destination =sys.argv[2]
-sources = os.listdir(folder)
+import os, re
+NGSrun=input("Specify the run name: ")
+# at the moment, the fasta files are in the Z drive and the folder sources of GLUE in Desktop
+origin_path = "z:/Antiviral Unit/Sequencing - HCV WGS/"+ NGSrun +"/FASTAs/"
+origin_directory = os.listdir(origin_path)
+#include something to double check the folder already exists
+destination_path = "c:/Users/juan.ledesma/Desktop/HCV_GLUE_AVU/sources/"+ NGSrun +"/"
+destination_directory = os.mkdir(destination_path)
 
-if folder[-1] != "/" and destination[-1] != "/":
-    folder += '/'
-    destination +='/'
-
-for sequence in sources:
+for sequence in origin_directory:
     if "PC" not in sequence:
-        fasta_file= folder + sequence
+        fasta_file= origin_path + sequence
         with open(fasta_file,'r') as fasta:
-            name=re.sub(r'^[0-9]*_',"",re.sub(r'-[0-9]',"",sequence))
-
-            file_name = destination + name 
-            #file_name = folder+'/'+ name 
+            #name=re.sub(r'^[0-9]*_',"",re.sub(r'-[0-9]',"",sequence))
+            name=re.sub(r'^[0-9]*_',"",sequence)
+            file_name = destination_path + name 
             renamed_fasta_file = open(file_name,'wt')
             renamed_sequence =""
             
