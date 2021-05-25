9038dd0765b49ee916129551ae038033e6f96707
Juan Ledesma <jledesmam@yahoo.es>
2021-05-25 17:18:31 +0100

    test#

--- python_script/glue_formatter.py
+++ python_script/glue_formatter.py
@@ -1,9 +1,17 @@
-import sys
-input_file = sys.argv[1] #excel
-
+import sys, os, re
 import pandas as pd
 from openpyxl import load_workbook
-molis_data = pd.read_excel(input_file)
+#once I know the fixed paths to the data, update this. 
+molis_file = sys.argv[1] #c:/Users/juan.ledesma/Desktop/HCV_GLUE_AVU/tabular/made-up_MolisData_test.xlsx 
+seq_files = sys.argv[2] #"c:/Users/juan.ledesma/Desktop/HCV_GLUE_AVU/sources/NGS91
+NGSrun = os.listdir(seq_files)
+sequences = []
+for seq in NGSrun:
+    seq=seq.replace(".fas","")
+    sequences.append(seq)
+print(sequences)
+
+molis_data = pd.read_excel(molis_file)
 #print(molis_data.dtypes[molis_data.dtypes == "datetime64[ns]"])
 datatypes = molis_data.dtypes.to_dict() # create a dictionary, col names are the keys and datatype the value
 #SAMPLE_DT        datetime64[ns]
@@ -15,12 +23,23 @@ for col, typ in datatypes.items():
 molis_data["LPERIOD"]=molis_data["LPERIOD"].apply('{:0>4}'.format) # 4 digit code
 molis_data["ORDNB"]=molis_data["ORDNB"].apply('{:0>4}'.format) # 4 digit code
 
-molis_data["MOLIS"]=molis_data["LID"].astype(str)+molis_data["LPERIOD"].astype(str)+molis_data["ORDNB"].astype(str)
+molis_data["MOLIS"]=molis_data["LID"].astype(str) + molis_data["LPERIOD"].astype(str) + molis_data["ORDNB"].astype(str)
+#molis_data["sequenceID"]=""
+#print(molis_data)
 
-GlueMolis=molis_data[["MOLIS","LID","LPERIOD","ORDNB","HCVGEN","SAMPLE_DT","RECEPT_DT","CORORDNB","ORDPATBIRTHDT","ORDPATSX"]]
+#GlueMolis=molis_data[["MOLIS","LID","LPERIOD","ORDNB","HCVGEN","SAMPLE_DT","RECEPT_DT","CORORDNB","ORDPATBIRTHDT","ORDPATSX"]]
 
-output_name= input_file.replace(".xlsx",".csv")
-GlueMolis.to_csv(output_name, index=False)
+#output_name= input_file.replace(".xlsx",".csv")
+#GlueMolis.to_csv(output_name, index=False)
+for seqid in sequences:
+    for m in molis_data["MOLIS"]:
+        if m in seqid:
+            molis_data.loc[molis_data["MOLIS"]==str(m), "sequenceID"] = seqid
+            
+        
+    #print(m, seqid)
+molis_data = molis_data.dropna(subset = ["sequenceID"])
+print(molis_data)
 
-
-
+#df.loc[df['c1'] == 'Value', 'c2'] = 10
+# It doesnt work for repeats 
