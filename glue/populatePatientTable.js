d43c3ee7a14b73cad3dec3bcf28f0a41d654cc2e
Juan Ledesma <jledesmam@yahoo.es>
2021-06-15 14:08:43 +0100

    javascript updated

---
+++ glue/populatePatientTable.js
@@ -0,0 +1,38 @@
+function nullTrim(string) {
+	if(string == null) {
+		return null;
+	}
+	return string.trim();
+}
+
+var patients;
+glue.inMode("module/tabularUtilityCsv", function(){
+    patients = glue.tableToObjects(glue.command(["load-tabular","tabular/made-up_MolisData_test_PATIENTS_NGS91.csv"]));
+}
+);
+_.each(patients, function(patient){
+    var patientId = nullTrim(patient["pid"]); 
+    var nhs = nullTrim(patient["NHS"]);  
+    var nhsid = nhs.toLowerCase(); // GLUE does not accept Upper cases for the headers so this two steps are to make the header of the file and the fields in custom table to match
+    var dob = nullTrim(patient["ORDPATBIRTHDT"]);
+    var sx = nullTrim(patient["ORDPATSX"]); 
+    //var age_diagnosis; 
+    // var city_of_residence;
+    // var country_of_birth;    
+    // var ethnicity;            
+    // var nationality; 
+
+    glue.command(["create", "custom-table-row", "patient", patientId]); //create custom-table-row samples <rowId> this will be the key, as it is molis you shoudl not add molis again
+    glue.inMode("custom-table-row/patient/"+patientId, function(){ // access the row to set the fields
+    glue.command(["set", "field", "nhs", nhs ]);  // hcvgen       molis        nhs          recept_dt    sample_dt --> fields in the custom table
+	glue.command(["set", "field", "date_of_birth", dob]);
+    glue.command(["set", "field", "gender", sx]);
+    //glue.command(["add", "link-target", "molis_ids","custom-table-row/molis_ids/"+molisName]); 
+    //glue.command(["set", "link", "nhs", nhsName]);
+    // add link-target sequence sequence/ncbi-refseqs/ i need to create the link with sequence, regular experssion to get the molis number only and link to samples
+    }
+    );
+    
+}
+);
+
---
+++ glue/populateSampleTable.js
@@ -0,0 +1,45 @@
+function nullTrim(string) {
+	if(string == null) {
+		return null;
+	}
+	return string.trim();
+}
+
+var samples;
+
+// access module tabularUtilityCsv and load data from file
+glue.inMode("module/tabularUtilityCsv", function(){
+    samples = glue.tableToObjects(glue.command(["load-tabular","tabular/made-up_MolisData_test_SAMPLES_NGS91.csv"]));
+}
+);
+// glue.logInfo()
+// this is equivalent to 
+// modules/tabularUtilityCsv
+// load-tabular tabular/made-up_MolisData_test_SAMPLES_NGS91.csv
+// exit
+
+
+// _.each is used to iterate over a list (samples) using the function
+// every single item from samples is going to be modify with a firt varaible and a second one.
+// second one is usually for the header, and first one should be fine for the values
+_.each(samples, function(sample){
+    var molisId = nullTrim(sample["MOLIS"]); // 
+    var sampleDate = nullTrim(sample["SAMPLE_DT"]); // this will be the value in the iteration
+    var receptionDate = nullTrim(sample["RECEPT_DT"]);
+    var initialGenotype = nullTrim(sample["HCVGEN"]);
+    var patientId = nullTrim(sample["patient_id"]);// set links not fields
+    
+    glue.command(["create", "custom-table-row", "sample", molisId]); //create custom-table-row samples <rowId> this will be the key, as it is molis you shoudl not add molis again
+    glue.inMode("custom-table-row/sample/"+molisId, function(){ // access the row to set the fields
+        //glue.command(["set", "field", "XXX",XXXX ]);  // hcvgen       molis        nhs          recept_dt    sample_dt --> fields in the custom table
+		glue.command(["set", "field", "sample_date", sampleDate]);
+        glue.command(["set", "field", "reception_date", receptionDate]);
+        glue.command(["set", "field", "initial_genotype", initialGenotype]);
+        //glue.command(["set", "field", "patient", patientid]); // NOT SURE ABOUT THIS 
+        glue.command(["set", "link-target", "patient", "custom-table-row/patient/"+patientId]);
+        //glue.command(["set", "link-target", "hospital", "custom-table-row/hospital/"+XXXX]);
+        //set link-target samples custom-table-row/sample/H210380785
+    }
+    );
+}
+);
