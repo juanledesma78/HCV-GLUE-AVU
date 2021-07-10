36afc709d1baea2642c8bcbdb7d5c0de17d697fb
Juan Ledesma <jledesmam@yahoo.es>
2021-07-10 15:38:03 +0100

    script to populate hospital data

---
+++ glue/populateHospitalTable.js
@@ -0,0 +1,23 @@
+function nullTrim(string) {
+	if(string == null) {
+		return "-"; //return null;
+	}
+	return string.trim();
+}
+
+var hospitals;
+glue.inMode("module/tabularUtilityCsv", function(){
+    hospitals = glue.tableToObjects(glue.command(["load-tabular","tabular/table_hospital/hospital_list.csv"]));
+}
+);
+
+_.each(hospitals, function(hospital){
+        var hospitalId = nullTrim(hospital["CORNB"]); 
+        var hospitalName = nullTrim(hospital["CORNB_FULLNAME"]);  
+        
+        glue.command(["create", "custom-table-row", "hospital", hospitalId]);
+        glue.inMode("custom-table-row/hospital/"+hospitalId, function(){ // access the row to set the fields
+            glue.command(["set", "field", "hospital_name", hospitalName ]);  
+            }); 
+        }
+    );
