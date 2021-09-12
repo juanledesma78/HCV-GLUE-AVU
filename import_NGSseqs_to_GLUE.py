db829c3df6bd35ee7706883eb2f4ec9812b9aa99
Juan.Ledesma <juan.ledesma@phe.gov.uk>
2021-09-12 12:07:07 +0100

    argparse library added and PEP 8 style

--- import_NGSseqs_to_GLUE.py
+++ import_NGSseqs_to_GLUE.py
@@ -1,31 +1,51 @@
-import os, re, sys
+import os
+import re
+import argparse
 import pandas as pd 
 import subprocess
-path_to_NGS_run  = sys.argv[1] #select the NGS run to import "/FASTAs/"
+
+program_specs = argparse.ArgumentParser(
+    prog='import_NGSseqs_to_GLUE.py',
+    usage='%(prog)s -input path-to-raw-NGSrun',
+    description='Import NGS sequences from a specified directory to sources directory and then transfer them to GLUE')
+program_specs.add_argument('-input','-i', help='path to the directory where the FASTQ files ares',required=True)
+args = program_specs.parse_args()
+
+
+path_to_ngs_run = args.input
+print(path_to_ngs_run)
+
+#path_to_NGS_run  = sys.argv[1] #select the NGS run to import "/FASTAs/"
 path_to_sources = "/home/phe.gov.uk/juan.ledesma/gluetools/projects/hcv_glue_avu/sources/"
-NGS = re.search(r'NGS[0-9]*.*', path_to_NGS_run) 
-NGS_fastas = NGS.group() # NGS91/FASTAs/
-ngsRun = re.search(r'^NGS[0-9]*.*[^/FASTAs]', NGS_fastas ) # keep the original name
-runName = ngsRun.group() 
-runName = runName.replace(' ','_') # to allow the glue commands, error if spaces
-NGSid = re.search(r'^NGS[0-9]*', runName ) # use only RUN name 
-runID = NGSid.group()
+NGS = re.search(r'NGS[0-9]*.*', path_to_ngs_run) 
+ngs_fastas = NGS.group() # NGS91/FASTAs/
+ngs_run = re.search(r'^NGS[0-9]*.*[^/FASTAs]', ngs_fastas ) # keep the original name
+run_name = ngs_run.group() 
+run_name = run_name.replace(' ','_') # to allow the glue commands, error if spaces
+ngs_id = re.search(r'^NGS[0-9]*', run_name ) # use only RUN name 
+run_id = ngs_id.group()
 
-if path_to_NGS_run[-1]!='/':
-    path_to_NGS_run= path_to_NGS_run +"/"
+#if path_to_NGS_run[-1]!='/':
+#    path_to_NGS_run= path_to_NGS_run +"/"
+#os.mkdir(path_to_sources + str(runName)) #sources_folder = os.mkdir(path_to_sources + str(runName)), not needed to assign a variable
 
-os.mkdir(path_to_sources + str(runName)) #sources_folder = os.mkdir(path_to_sources + str(runName)), not needed to assign a variable
+#this substitute the code above
+sources_directory = os.path.join(path_to_sources,run_name)
+os.mkdir(sources_directory) #os.mkdir(os.path.join(path_to_sources,runName))
 
-NGS_directory = os.listdir(path_to_NGS_run) # list the files in the directory
+ngs_directory = os.listdir(path_to_ngs_run) # list the files in the directory
 
-for sequence in NGS_directory:
-    fasta_file= path_to_NGS_run + sequence
+
+for sequence in ngs_directory:
+    fasta_file = os.path.join(path_to_ngs_run,sequence)
     with open(fasta_file,'r') as fasta:
         fasta_name=re.sub(r'^[0-9]*_',"",sequence) # remove first digits from the header
-        fasta_name=fasta_name.replace(".fas", "_" + runID ) # replace .fas with NGS run
+        fasta_name=fasta_name.replace(".fas", "_" + run_id ) 
         final_name=fasta_name.replace(".", "-") # H211900006.2_NGS99 and H211140566.1_NGS96it contained . rather than - (manual modification?)
         file_name = final_name + ".fas"
-        renamed_fasta_file = open(path_to_sources + runName +"/"+ file_name,'wt') # final_name + ".fas"
+        #renamed_fasta_file = open(path_to_sources + runName +"/"+ file_name,'wt') # final_name + ".fas"
+        source_fasta_file = os.path.join(path_to_sources,run_name, file_name)
+        renamed_fasta_file = open(source_fasta_file,'wt')
         renamed_sequence =""
         for line in fasta:
             if ">" in line: 
@@ -37,19 +57,20 @@ for sequence in NGS_directory:
     
 
 #CALLING GLUE TO IMPORT NEW FASTA SEQUENCES python 3.8
-Glue_commands = """
-project hcv_glue_avu
-import source sources/RUNID
-Q
-exit
-quit
-\
-"""
+glue_commands = """
+    project hcv_glue_avu
+    import source sources/RUNID
+    Q
+    exit
+    quit
+    \
+    """
 #project hcv_glue_avu\nimport source sources/NGS93\nQ\nexit\n'
-Glue_commands = Glue_commands.replace("RUNID", runName) 
-p1 = subprocess.run("gluetools.sh" , text=True, input=Glue_commands)
+glue_commands = glue_commands.replace("RUNID", run_name) 
+p1 = subprocess.run("gluetools.sh" , text=True, input=glue_commands)
 
 if p1.returncode == 0:
     print("\nSequences successfully imported in GLUE\n")
 else:
     print("\nSomething went wrong")
+
--- populate_metadata_from_molis.py
+++ populate_metadata_from_molis.py
@@ -1,5 +1,8 @@
 import sqlite3
-import sys, os, re, subprocess
+import os
+import re
+import subprocess
+import argparse
 import pandas as pd
 
 conn = sqlite3.connect("/home/phe.gov.uk/juan.ledesma/gluetools/projects/hcv_glue_avu/tabular/molis.db")
@@ -28,33 +31,40 @@ HAVING STAT_ORD.REC_SITE_NB IN ("SHC", "SHE")
 ;
 ''', conn)
 
-sources_run = sys.argv[1] #"..sources/NGS91 ...
+#sources_run = sys.argv[1] #"..sources/NGS91 ...
+specs = argparse.ArgumentParser(
+    prog='populate_metadata_from_molis.py', 
+    usage='%(prog)s --sources/-s path-to-sources-NGSrun', 
+    description='populate information from molis database to glue porject')
+specs.add_argument('--sources','-s', required=True, help=' path to the folder in sources/ that contains the fasta sequences which data is going to be transferred')
+sources_run =specs.parse_args()
+# Namespace(sources='../sources/NGS91')
 
-SourcesNGSid = re.search(r'NGS[0-9]*.*[^/]',sources_run)
-SourcesNGSid = SourcesNGSid.group() # this for calling GLUE NGS94_(repeat_Miseq_NGS93) 
-NGSid = re.search(r'NGS[0-9]*',SourcesNGSid)
-NGSid = NGSid.group() # this for csv NGS94
+sources_ngs_id = re.search(r'NGS[0-9]*.*[^/]',sources_run.sources)
+sources_ngs_id = sources_ngs_id.group() # this for calling GLUE NGS94_(repeat_Miseq_NGS93) 
+ngs_id = re.search(r'NGS[0-9]*',sources_ngs_id)
+ngs_id = ngs_id.group() # this for csv NGS94
 
 """ Create a table with sequenceID and molis to check against synthetic_molis.db """
-NGSsequences = os.listdir(sources_run)
+ngs_sequences = os.listdir(sources_run.sources)
 sequences = []
 molis_id = []
-Pipeline = []
-Version = []
-for seq in NGSsequences:
+pipeline = []
+version = []
+for seq in ngs_sequences:
     seq = seq.replace(".fas","")
     mol = re.sub(r'-[0-9].*',"", seq)
     sequences.append(seq)
     molis_id.append(mol)
 
     if "genomancer" in seq:
-        Pipeline.append("Genomacer")
-        Version.append("v.1")
+        pipeline.append("Genomacer")
+        version.append("v.1")
     else:
-        Pipeline.append("PHE HCV bioinformatics pipeline")
-        Version.append("version 1.0")
+        pipeline.append("PHE HCV bioinformatics pipeline")
+        version.append("version 1.0")
 
-run = {"sequenceID" : sequences, "MOLIS" : molis_id, "Pipeline": Pipeline, "Version": Version}
+run = {"sequenceID" : sequences, "MOLIS" : molis_id, "Pipeline": pipeline, "Version": version}
 ngs_run = pd.DataFrame(data=run) 
 
 
@@ -64,8 +74,8 @@ molis_data["RECEPT_DT"]= pd.to_datetime(molis_data["RECEPT_DT"])
 molis_data["ORDPATBIRTHDT"]= pd.to_datetime(molis_data["ORDPATBIRTHDT"])
 #molis_data["RECEPT_DT"] = molis_data["RECEPT_DT"].dt.strftime('%d-%b-%Y')
 datatypes = molis_data.dtypes.to_dict() # create a dictionary, colnames are the keys and datatype the value
-#SAMPLE_DT        datetime64[ns]
-#change data format to fit GLUE
+                                        #SAMPLE_DT        datetime64[ns]
+                                        #change data format to fit GLUE
 for col, typ in datatypes.items():
     if (typ == "datetime64[ns]"):
         molis_data[col] = molis_data[col].dt.strftime('%d-%b-%Y') 
@@ -75,55 +85,58 @@ molis_data["ORDPATSX"] = molis_data["ORDPATSX"].replace({'F': 'Female', 'M': 'Ma
 
 """match the sequenceID (or molis numbers) with the molis number from the database to filter the run info""" 
 #sequenceId_molis_info = ngs_run.merge(molis_data, how='left', left_on='MOLIS', right_on='MOLIS')
-sequenceId_molis_info = ngs_run.merge(molis_data, on='MOLIS')
+sequence_id_molis_info = ngs_run.merge(molis_data, on='MOLIS')
 
 path_to_tabular = "/home/phe.gov.uk/juan.ledesma/gluetools/projects/hcv_glue_avu/tabular/"
-sample_list= path_to_tabular + "table_sample/" + "metadata_table_SAMPLE_" + NGSid + ".csv"
-patient_list= path_to_tabular + "table_patient/" +  "metadata_table_PATIENT_" + NGSid + ".csv"
-sequence_list = path_to_tabular +"table_sequence/"+ "metadata_table_SEQUENCE_"+ NGSid +".csv"
+sample_list= os.path.join(path_to_tabular,"table_sample","metadata_table_SAMPLE_" + ngs_id + ".csv") # each comma add a slash /
+patient_list= os.path.join(path_to_tabular ,"table_patient","metadata_table_PATIENT_" + ngs_id + ".csv")
+sequence_list = os.path.join(path_to_tabular +"table_sequence", "metadata_table_SEQUENCE_"+ ngs_id +".csv")
+
+
 #sequenceId_molis_info[["MOLIS","LID","LPERIOD","ORDNB","HCVGEN","SAMPLE_DT","RECEPT_DT","pid","NHS", "CORORDNB","REFEXT"]].drop_duplicates("MOLIS").to_csv(sample_list, index=False)
 # the id for the patient can be NHS, patientid or anyother id according to what I was told. Molis Housekeeping
-sequenceId_molis_info[["MOLIS","LID","LPERIOD","ORDNB","HCVGEN","SAMPLE_DT","RECEPT_DT","ORDPATIDNB", "CORORDNB","REFEXT"]].drop_duplicates("MOLIS").to_csv(sample_list, index=False)
-sequenceId_molis_info[["ORDPATIDNB", "ORDPATBIRTHDT","ORDPATSX"]].drop_duplicates("ORDPATIDNB").to_csv(patient_list, index=False)
-sequenceId_molis_info[["sequenceID","MOLIS","Pipeline","Version","ORDPATIDNB"]].to_csv(sequence_list, index=False) # ORDPATIDNB added here in case shcema needs to be changed 
+sequence_id_molis_info[["MOLIS","LID","LPERIOD","ORDNB","HCVGEN","SAMPLE_DT","RECEPT_DT","ORDPATIDNB", "CORORDNB","REFEXT"]].drop_duplicates("MOLIS").to_csv(sample_list, index=False)
+sequence_id_molis_info[["ORDPATIDNB", "ORDPATBIRTHDT","ORDPATSX"]].drop_duplicates("ORDPATIDNB").to_csv(patient_list, index=False)
+sequence_id_molis_info[["sequenceID","MOLIS","Pipeline","Version","ORDPATIDNB"]].to_csv(sequence_list, index=False) # ORDPATIDNB added here in case shcema needs to be changed 
 
 # CALL GLUE TO IMPORT THIS TABLES AND SEQUENCE TABLE
 # update the name run in javascripts to allow the data population
 
-new_NGS_run_PATIENT = ""
+new_ngs_run_patient = ""
 with open("/home/phe.gov.uk/juan.ledesma/gluetools/projects/hcv_glue_avu/glue/populatePatientTable.js",'r') as script:
         text = script.read()
-        new_NGS_run_PATIENT = re.sub(r'metadata_table_PATIENT_[a-zA-Z0-9]*.csv','metadata_table_PATIENT_'+ NGSid + '.csv', text)
+        new_ngs_run_patient = re.sub(r'metadata_table_PATIENT_[a-zA-Z0-9]*.csv','metadata_table_PATIENT_'+ ngs_id + '.csv', text)
         script.close()
 with open("/home/phe.gov.uk/juan.ledesma/gluetools/projects/hcv_glue_avu/glue/populatePatientTable.js",'w') as script:
-    script.write(new_NGS_run_PATIENT)
+    script.write(new_ngs_run_patient)
     script.close()
 
-new_NGS_run_SAMPLE = ""
-with open("/home/phe.gov.uk/juan.ledesma/gluetools/projects/hcv_glue_avu/glue/populateSampleTable.js",'r') as script: # THIS IS NOT THE FINAL PATH AS IT DEPENDS ON WHERE GLUE IS GOING TO BE INSTALLED
+new_ngs_run_sample = ""
+with open("/home/phe.gov.uk/juan.ledesma/gluetools/projects/hcv_glue_avu/glue/populateSampleTable.js",'r') as script: 
         text = script.read()
-        new_NGS_run_SAMPLE = re.sub(r'metadata_table_SAMPLE_[a-zA-Z0-9]*.csv','metadata_table_SAMPLE_'+ NGSid + '.csv', text)
+        new_ngs_run_sample = re.sub(r'metadata_table_SAMPLE_[a-zA-Z0-9]*.csv','metadata_table_SAMPLE_'+ ngs_id + '.csv', text)
         script.close()
 with open("/home/phe.gov.uk/juan.ledesma/gluetools/projects/hcv_glue_avu/glue/populateSampleTable.js",'w') as script:
-    script.write(new_NGS_run_SAMPLE)
+    script.write(new_ngs_run_sample)
     script.close()
 
 
-Glue_commands = """ 
-project hcv_glue_avu
-run script glue/populatePatientTable.js
-run script glue/populateSampleTable.js
-module NGSSequenceDataPopulator populate --whereClause "source.name='NGSsource'" --fileName tabular/table_sequence/metadata_table_SEQUENCE_RUNID.csv
-Q
-exit
-quit
-\
-"""
+glue_commands = """ 
+    project hcv_glue_avu
+    run script glue/populatePatientTable.js
+    run script glue/populateSampleTable.js
+    module NGSSequenceDataPopulator populate --whereClause "source.name='NGSsource'" --fileName tabular/table_sequence/metadata_table_SEQUENCE_RUNID.csv
+    Q
+    exit
+    quit
+    \
+    """
 #project hcv_glue_avu\nimport source sources/NGS93\nQ\nexit\n'
-Glue_commands = Glue_commands.replace("NGSsource", SourcesNGSid) #NGSid)
-Glue_commands = Glue_commands.replace("RUNID", NGSid) 
-p1 = subprocess.run("gluetools.sh" , text=True, input=Glue_commands)
+glue_commands = glue_commands.replace("NGSsource", sources_ngs_id) #NGSid)
+glue_commands = glue_commands.replace("RUNID", ngs_id) 
+p1 = subprocess.run("gluetools.sh" , text=True, input=glue_commands)
 if p1.returncode == 0:
     print("\nMetadata successfully uploaded in GLUE\n")
 else:
     print("\nSomething went wrong")
+
